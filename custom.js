74432cffd100c54a9ffce60790c1c99beada3e93
miloshsk <miloshsk@gmail.com>
2018-09-11 16:18:55 +0400

    Fix favorite error

--- custom.js
+++ custom.js
@@ -112,7 +112,7 @@ const FoodItemCtrl = (function(){
 		getFavoriteFood() {
 			let count = Object.create(null), max = 0, message = '', current;
 			for(let item of data.foodItems) {
-				if((current = count[item.title] = ~~count[item.tile] + 1) > max){
+				if((current = count[item.title] = ~~count[item.title] + 1) > max){
 					max = current;
 				}
 			}
