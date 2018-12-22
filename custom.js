817ce284ec7b6e5cb9a46f7b212aa146c17acac0
miloshsk <miloshsk@gmail.com>
2018-12-22 14:27:24 +0300

    Change structure

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 // Storage Controller
-const StorageCtrl = (function() {
+const StorageCtrl = (()=> {
 	return {
 		storeItem (foodItem) {
 			let foodItems;
@@ -24,39 +24,36 @@ const StorageCtrl = (function() {
 		},
 		deleteItemFromStorage(id) {
 			let items = JSON.parse(localStorage.getItem('foodItems'));
-			items.forEach(function(item, index) {
-				if(id === item.id) {
-					items.splice(index, 1);
-				}
-			});
+			const newId = items.findIndex((item) => {return item.id === id});
+			items.splice(newId,1);
 			localStorage.setItem('foodItems', JSON.stringify(items));
 		},
 		clearStorage() {
 			localStorage.removeItem('foodItems');
-		} 
+		}
 	}
 })();
 // Item Controller
-const FoodItemCtrl = (function(){
+const FoodItemCtrl = (() =>{
 	// Item Constructor
 	const FoodItem = function (id, title, cost) {
 		this.id = id;
 		this.title = title;
 		this.cost = cost;
-	}
+	};
 	// Data Structure
 	const data = {
 		foodItemID: 0,
 		foodItemTitle: '',
 		foodItemCost: 0,
 		foodItems: StorageCtrl.getItemsFromStorage()
-	}
+	};
 	return {
 		getItems () {
 			return data.foodItems;
 		},
-		setTitle () {
-			data.foodItemTitle = this.dataset.food;
+		setTitle (target) {
+			data.foodItemTitle = target.dataset.food;
 		},
 		setCost (cost) {
 			data.foodItemCost = cost;
@@ -76,28 +73,19 @@ const FoodItemCtrl = (function(){
 			return newFoodItem;
 		},
 		getId(e) {
-			const id = parseInt(e.target.closest('tr').getAttribute('data-id'));
-			return id;
+			return parseInt(e.target.closest('tr').getAttribute('data-id'));
 		},
 		removeItem (newId) {
-			const id = newId;
-			data.foodItems.forEach(function(item, index) {
-				if(item.id === id) {
-					data.foodItems.splice(index, 1);
-				}
-			});
+			const id = data.foodItems.findIndex((item) => {return item.id === newId});
+			data.foodItems.splice(id,1);
 		},
 		getTotalSum () {
-			let result = 0;
-			for(let item of data.foodItems) {
-				result += item.cost;
-			}
-			return result;
+			return data.foodItems.reduce((cur, next) => {
+				return cur + next.cost;
+			}, 0);
 		},
 		getTotalResult() {
-			const result = this.getTotalSum();
-			const message = `Total cost: ${result}`;
-			return message;
+			return `Total cost: ${this.getTotalSum()}`;
 		},
 		getAverageValue(){
 			const result = this.getTotalSum();
@@ -110,21 +98,20 @@ const FoodItemCtrl = (function(){
 			return message;
 		},
 		getFavoriteFood() {
-			let count = Object.create(null), max = 0, message = '', current;
+			let count = Object.create(null), max = 0, current;
 			for(let item of data.foodItems) {
 				if((current = count[item.title] = ~~count[item.title] + 1) > max){
 					max = current;
 				}
 			}
-			let result = Object.keys(count).filter(function(x) {
+			let result = Object.keys(count).filter((x)=> {
 				return count[x] === max;
 			});
-			if(result.length === 0) {
-				message = 'You haven\'t eaten anything yet';
+			if(!result.length) {
+				return 'You haven\'t eaten anything yet';
 			} else {
-				message = `Favorite food: ${result.join(', ')}`;
+				return `Favorite food: ${result.join(', ')}`;
 			}
-			return message;
 		},
 		clearItems() {
 			data.foodItems = [];
@@ -133,7 +120,7 @@ const FoodItemCtrl = (function(){
 })();
 
 // UI Controller
-const UICtrl = (function(){
+const UICtrl = (() =>{
 	const UISelectors = {
 		itemList: '#list',
 		foodTitle: '.title',
@@ -148,9 +135,22 @@ const UICtrl = (function(){
 		favoriteFoodBlock: '.fav-food',
 		clearBtn: '.clear',
 		message: '.message'
-	}
+	};
+	const UIElements = {
+		itemList: document.querySelector(UISelectors.itemList),
+		expense: document.querySelector(UISelectors.expense),
+		expenseValue: document.querySelector(UISelectors.expenseValue),
+		totalResultBlock: document.querySelector(UISelectors.totalResultBlock),
+		averageValueBlock: document.querySelector(UISelectors.averageValueBlock),
+		favoriteFoodBlock: document.querySelector(UISelectors.favoriteFoodBlock),
+		totalResultBtn: document.querySelector(UISelectors.totalResultBtn),
+		averageValueBtn: document.querySelector(UISelectors.averageValueBtn),
+		favoriteFoodBtn: document.querySelector(UISelectors.favoriteFoodBtn),
+		clearBtn: document.querySelector(UISelectors.clearBtn),
+		foodTitle: document.querySelectorAll(UISelectors.foodTitle)
+	};
 	return {
-		createMessage(message) {
+		createMessage (message) {
 			if(document.querySelector(UISelectors.message)) {
 				return false;
 			} else {
@@ -166,51 +166,47 @@ const UICtrl = (function(){
 		populateItemList (items){
 			let html = '';
 			items.forEach(function(item) {
-				html += `<tr data-id="${item.id}">
-						 <td>${item.title}</td>
-						 <td>${item.cost}</td>
-						 <td><button class="delete-item"></button></td></tr>`;
+				html += `<tr data-id="${item.id}"><td>${item.title}</td><td>${item.cost}</td><td><button class="delete-item"></button></td></tr>`;
 			});
-			document.querySelector(UISelectors.itemList).innerHTML = html;
+			UIElements.itemList.innerHTML = html;
 		},
-		additemToList (item) {
+		addItemToList (item) {
 			const row = document.createElement('tr');
 			row.dataset.id = item.id;
-			row.innerHTML = ` <td>${item.title}</td>
-						 <td>${item.cost}</td>
-						 <td><button class="delete-item"></button></td>`;
-			document.querySelector(UISelectors.itemList).appendChild(row);
+			row.innerHTML = `<td>${item.title}</td><td>${item.cost}</td><td><button class="delete-item"></button></td>`;
+			UIElements.itemList.appendChild(row);
 		},
 		getSelectors () {
 			return UISelectors;
 		},
+		getUIElements() {
+			return UIElements;
+		},
 		showExpenseForm () {
-			document.querySelector(UISelectors.expense).style.display = 'block';
+			UIElements.expense.style.display = 'block';
 		},
 		getCostValue () {
-			const costValue = document.querySelector(UISelectors.expenseValue).value;
-			let result;
+			const costValue = UIElements.expenseValue.value;
 			if(costValue) {
-				result = parseInt(costValue);
+				return parseInt(costValue);
 			} else {
-				result = 0
-			} 
-			return result;
+				return  0
+			}
 		},
 		hideExpenseForm () {
-			document.querySelector(UISelectors.expense).style.display = 'none';
-			document.querySelector(UISelectors.expenseValue).value = '';
+			UIElements.expense.style.display = 'none';
+			UIElements.expenseValue.value = '';
 		},
 		removeItemRow (e) {
-			if(e.target.className == UISelectors.deleteItem.slice(1)) {
+			if(e.target.className === UISelectors.deleteItem.slice(1)) {
 				e.target.closest('tr').remove();
 			}
 		},
-		showTotal (selector, message) {
-			document.querySelector(selector).textContent = message;
+		showTotal (element, message) {
+			element.textContent = message;
 		},
 		clearFoodItems () {
-			const list = document.querySelector(UISelectors.itemList);
+			const list = UIElements.itemList;
 			while(list.firstChild) {
 				list.removeChild(list.firstChild);
 			}
@@ -220,33 +216,32 @@ const UICtrl = (function(){
 
 // App Controller
 
-const App = (function(FoodItemCtrl, UICtrl, StorageCtrl){
-	let items, UISelectors;
-	const loadEventListeners = function() {
-		UISelectors = UICtrl.getSelectors();
-		Array.from(document.querySelectorAll(UISelectors.foodTitle)).forEach(function(item) {
-			item.addEventListener('click',openExpenseForm );
+const App = ((FoodItemCtrl, UICtrl, StorageCtrl) =>{
+	const UIElements = UICtrl.getUIElements();
+	const loadEventListeners = () => {
+		Array.from(UIElements.foodTitle).forEach(function (item) {
+			item.addEventListener('click', openExpenseForm);
 		});
-		document.querySelector(UISelectors.expense).addEventListener('submit',createFoodItemRow );
-		document.querySelector(UISelectors.itemList).addEventListener('click',removeItemRow);
-		document.querySelector(UISelectors.totalResultBtn).addEventListener('click', showTotalCost );
-		document.querySelector(UISelectors.averageValueBtn).addEventListener('click', showAverageValue);
-		document.querySelector(UISelectors.favoriteFoodBtn).addEventListener('click',showFavoriteFood );
-		document.querySelector(UISelectors.clearBtn).addEventListener('click',clearAll );
-	}
-	const showTotalCost = function() {
+		UIElements.expense.addEventListener('submit', createFoodItemRow);
+		UIElements.itemList.addEventListener('click', removeItemRow);
+		UIElements.totalResultBtn.addEventListener('click', showTotalCost);
+		UIElements.averageValueBtn.addEventListener('click', showAverageValue);
+		UIElements.favoriteFoodBtn.addEventListener('click', showFavoriteFood);
+		UIElements.clearBtn.addEventListener('click', clearAll);
+	};
+	const showTotalCost = () => {
 		const result = FoodItemCtrl.getTotalResult();
-		UICtrl.showTotal(UISelectors.totalResultBlock, result);
-	}
-	const showAverageValue = function() {
+		UICtrl.showTotal(UIElements.totalResultBlock, result);
+	};
+	const showAverageValue = () => {
 		const averageVal = FoodItemCtrl.getAverageValue();
-		UICtrl.showTotal(UISelectors.averageValueBlock, averageVal);
-	}
-	const showFavoriteFood = function() {
+		UICtrl.showTotal(UIElements.averageValueBlock, averageVal);
+	};
+	const showFavoriteFood = () => {
 		const favoriteFood = FoodItemCtrl.getFavoriteFood();
-		UICtrl.showTotal(UISelectors.favoriteFoodBlock, favoriteFood);
-	}
-	const clearAll = function() {
+		UICtrl.showTotal(UIElements.favoriteFoodBlock, favoriteFood);
+	};
+	const clearAll = () => {
 		FoodItemCtrl.clearItems();
 		UICtrl.hideExpenseForm();
 		UICtrl.clearFoodItems();
@@ -254,37 +249,36 @@ const App = (function(FoodItemCtrl, UICtrl, StorageCtrl){
 		showTotalCost();
 		showAverageValue();
 		showFavoriteFood();
-	}
-	const removeItemRow = function(e) {
+	};
+	const removeItemRow = (e) => {
 		const id = FoodItemCtrl.getId(e);
 		UICtrl.removeItemRow(e);
 		FoodItemCtrl.removeItem(id);
 		StorageCtrl.deleteItemFromStorage(id);
-	}
-	const createFoodItemRow = function(e) {
+	};
+	const createFoodItemRow = (e) =>{
 		e.preventDefault();
 		const cost = UICtrl.getCostValue();
 		if(cost) {
 			FoodItemCtrl.setCost(cost);
 			UICtrl.hideExpenseForm();
 			const newFoodItem = FoodItemCtrl.addItem();
-			UICtrl.additemToList(newFoodItem);
+			UICtrl.addItemToList(newFoodItem);
 			StorageCtrl.storeItem(newFoodItem);
 		} else {
 			UICtrl.createMessage('Enter value');
 		}
-		
-	}
-	const openExpenseForm = function() {
+	};
+	const openExpenseForm = (e) =>{
 		FoodItemCtrl.setID();
-		FoodItemCtrl.setTitle.call(this);
+		FoodItemCtrl.setTitle(e.target);
 		UICtrl.showExpenseForm();
-	}
+	};
 	return {
-		init: function(){
-			items = FoodItemCtrl.getItems();
-			UICtrl.populateItemList(items);
+		init: () => {
 			loadEventListeners();
+			const items = FoodItemCtrl.getItems();
+			UICtrl.populateItemList(items);
 		}
 	}
 })(FoodItemCtrl, UICtrl, StorageCtrl);
